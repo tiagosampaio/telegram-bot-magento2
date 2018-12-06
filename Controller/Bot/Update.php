1bf0ba9cb478abd649bfd7c64ae000cad419c48b
Tiago Sampaio <tiago@tiagosampaio.com>
2018-12-06 20:50:20 -0200

    More code testing.

--- Controller/Bot/Update.php
+++ Controller/Bot/Update.php
@@ -6,7 +6,6 @@ namespace Telegram\Bot\Controller\Bot;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\ResponseInterface;
 
 /**
  * Class Update
@@ -24,13 +23,20 @@ class Update extends Action
      */
     private $serializer;
 
+    /**
+     * @var \Telegram\ObjectType\Entity\UpdateFactory
+     */
+    private $updateFactory;
+
     public function __construct(
         Context $context,
         \Telegram\Bot\Api\ServiceApiInterface $api,
+        \Telegram\ObjectType\Entity\UpdateFactory $updateFactory,
         \Magento\Framework\Serialize\SerializerInterface $serializer
     ) {
         parent::__construct($context);
         $this->api = $api;
+        $this->updateFactory = $updateFactory;
         $this->serializer = $serializer;
     }
 
@@ -39,16 +45,19 @@ class Update extends Action
         $json = $this->getRequest()->getContent();
         $data = $this->serializer->unserialize($json);
 
-        $chatId    = $data['message']['chat']['id'];
-        $userId    = $data['message']['from']['id'];
-        $firstName = $data['message']['from']['first_name'];
-        $lastName  = $data['message']['from']['last_name'];
+        $token  = '608782915:AAFym6-ECFA0LfF6HLbZc_MHHM-vKkCCSjE';
+        $api = $this->api->get($token);
+
+        /** @var \Telegram\ObjectType\Entity\UpdateInterface $update */
+        $update = $this->updateFactory->create(['data' => $data]);
+
+        $chatId    = $update->getMessage()->getChat()->getId();
+        $userId    = $update->getMessage()->getFrom()->getId();
+        $firstName = $update->getMessage()->getFrom()->getFirstName();
+        $lastName  = $update->getMessage()->getFrom()->getLastName();
 
         $message = __('Hey %1 %2, how are you doing? This is our automatic response to your message.', $firstName, $lastName);
 
-        $token  = '608782915:AAFym6-ECFA0LfF6HLbZc_MHHM-vKkCCSjE';
-
-        $api = $this->api->get($token);
         $api->methods()->sendMessage($chatId, (string) $message);
     }
 }
